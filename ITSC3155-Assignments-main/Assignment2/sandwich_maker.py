b2bcf17fd93b7268b7468e20bdd9c5cd992de982
Emely Ruiz Almaguer <eruizalm@charlotte.edu>
2026-02-17 12:07:37 -0500

    feat: sandwich_maker class completed

--- ITSC3155-Assignments-main/Assignment2/sandwich_maker.py
+++ ITSC3155-Assignments-main/Assignment2/sandwich_maker.py
@@ -5,7 +5,12 @@ class SandwichMaker:
 
     def check_resources(self, ingredients):
         """Returns True when order can be made, False if ingredients are insufficient."""
-        #####
-
+        for ingredient in ingredients.keys():
+            if ingredients[ingredient] > self.machine_resources[ingredient]:
+                print("Sorry not enough", ingredient)
+                return False
+        return True
     def make_sandwich(self, sandwich_size, order_ingredients):
-        ########
+        for ingredient, amount in recipes[sandwich_size][order_ingredients].items():
+            self.machine_resources[ingredient] -= amount
+        print(sandwich_size, "sandwich is ready. Bon apetit!")
